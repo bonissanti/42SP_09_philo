d203809500343e5d1939d15e2f97d1f4b0dbe09d
Bruno Onissanti <brunoonissanti@gmail.com>
2024-01-21 18:33:43 -0300

    new philo with structs and without static structure

--- philo/src/threads.c
+++ philo/src/threads.c
@@ -1,18 +1,18 @@
 #include "../include/philo.h"
 
-// void	start_threads(t_philo *philos, t_status *status)
-// {
-// 	int 		i;
-// 	pthread_t	*threads;
+void	start_threads(t_philo *philos, t_status *status)
+{
+	int 		i;
+	pthread_t	*threads;
 
-// 	i = -1;
-// 	threads = ft_calloc(status->nbr_philo, sizeof(pthread_t));
-// 	if (!threads)
-// 		return ;
+	i = -1;
+	threads = ft_calloc(status->nbr_philo, sizeof(pthread_t));
+	if (!threads)
+		return ;
 	
-// 	while (++i < status->nbr_philo)
-// 	{
-// 		if (pthread_create(&threads[i], NULL, &routine, &philos[i]))
-// 			printf("Error: thread creation failed\n");
-// 	}
-// }
+	while (++i < status->nbr_philo)
+	{
+		if (pthread_create(&threads[i], NULL, &philo_routine, &philos[i]))
+			printf("Error: thread creation failed\n");
+	}
+}
